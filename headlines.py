106f7dc7dd00071617ee5c48cb2e5a3432b2f83c
amilberger95 <amilberger95@gmail.com>
2017-02-05 22:47:56 -0500

    using GET

--- headlines.py
+++ headlines.py
@@ -1,5 +1,5 @@
 import feedparser
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,15 +7,28 @@ feeds = {
 		'cnn': 'http://rss.cnn.com/rss/edition.rss',
         'fox': 'http://feeds.foxnews.com/foxnews/latest',
         'bbc': 'http://feeds.bbci.co.uk/news/rss.xml',
-        'cnbc': 'http://www.cnbc.com/id/100003114/device/rss/rss.xml'}
+        'cnbc': 'http://www.cnbc.com/id/100003114/device/rss/rss.xml',
+        'freep': 'http://rssfeeds.freep.com/freep/home&x=1',
+        'iol': 'http://www.iol.co.za/cmlink/1.640'
+        }
 
 
-@app.route("/")
-def nav():
-	return render_template("layout.html")
+# @app.route("/")
+# def nav():
+# 	return render_template("layout.html")
 
-@app.route("/<publication>")
-def get_news(publication='bbc', params=['title', 'published', 'summary']):
+# @app.route("/<publication>")
+# def get_news(publication='bbc', params=['title', 'published', 'summary']):
+# 	feed = feedparser.parse(feeds[publication])
+# 	return render_template("layout.html", articles=feed['entries'], n = publication)
+
+@app.route('/')
+def get_news():
+	query = request.args.get('publication')
+	if not query or query.lower() not in feeds:
+		publication = 'bbc'
+	else:
+		publication = query.lower()
 	feed = feedparser.parse(feeds[publication])
 	return render_template("layout.html", articles=feed['entries'], n = publication)
 
